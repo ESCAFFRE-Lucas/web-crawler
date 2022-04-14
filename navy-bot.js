9a86e72bd31cb5526d9431e65563039dce4b8cad
LESCAFFRE <lucas.escaffre@ynov.com>
2022-04-14 17:14:22 +0200

    discord bot send message in specific channel at specific time

--- navy-bot.js
+++ navy-bot.js
@@ -1,13 +1,25 @@
 require('dotenv').config()
+const schedule = require('node-schedule');
+
 // Import relevant classes from discord.js
 const { Client, Intents } = require('discord.js');
+
 // Instantiate a new client with some necessary parameters.
 const client = new Client(
     { intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] }
 );
-// Notify progress
-client.on('ready', function(e){
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+
 // Authenticate
-client.login(process.env.DISCORD_TOKEN)
+client.login(process.env.DISCORD_TOKEN);
+
+client.once("ready", () => {
+  console.log(`Online as ${client.user.tag}`);
+
+ new schedule.scheduleJob('00 13 17 * * *', () => {
+    if (client.channels.cache.get('964164221321490462').isText) {
+      client.channels.cache.get('964164221321490462').send('hey');
+    }
+  });
+});
+
+
