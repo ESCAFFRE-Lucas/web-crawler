85f07de137b8fd678cc8491c0aa3203788c9b8a7
LESCAFFRE <lucas.escaffre@ynov.com>
2022-04-15 15:34:33 +0200

    get data from hyperplanning

--- src/start.ts
+++ src/start.ts
@@ -9,5 +9,10 @@ const puppeteer = require('puppeteer');
   await (await page.waitForSelector('input[id="username"]')).type('lucas.escaffre@ynov.com');
   await (await page.waitForSelector('input[id="password"]')).type('Lucas20030309');
   await (await page.waitForSelector('input[value="Connexion"]')).click();
+  await page.waitForSelector('#id_98_cont0 > div:nth-child(1)');
+  const subject = await page.$eval('#id_98_cont0 > div:nth-child(1)', (ele:  Element) => {
+    return ele.innerHTML;
+  })
+  console.log(subject);
 })();
 
