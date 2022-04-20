88ecd1869b442cb17ed26a2cd2812c5c05c18946
LESCAFFRE <lucas.escaffre@ynov.com>
2022-04-20 13:57:02 +0200

    go to do a 3 branch

--- src/start.ts
+++ src/start.ts
@@ -14,20 +14,6 @@ import { bot } from './navy-bot';
   await (await page.waitForSelector('input[id="username"]')).type('lucas.escaffre@ynov.com');
   await (await page.waitForSelector('input[id="password"]')).type(process.env.PASSWORD);
   await (await page.waitForSelector('input[value="Connexion"]')).click();
-  // const trueSubject = await page.$$eval('#GInterface\\.Instances\\[1\\]\\.Instances\\[9\\]_Grille_Elements', async (ele: Element[]) => {
-  //   console.log(...ele);
-  //   // ele.forEach((allitem) => {
-  //   //   if (window.getComputedStyle(allitem).left === '375px' && dayName === 'Dimanche') {
-  //   //     return page.$$eval('#id_98_cont4 > div:not(:nth-child(3))', (ele:  Element[]) => {
-  //   //       return ele.map((item) => {
-  //   //         console.log(item.innerHTML);
-  //   //         return item.innerHTML;
-  //   //       });
-  //   //     });
-  //   //   }
-  //   // })
-  // });
-  // console.log(trueSubject);
   await page.waitForSelector('#id_98_cont2')
   const subject = await page.$$eval('#id_98_cont2 > div:not(:nth-child(3))', (ele:  Element[]) => {
     return ele.map((item) => {
