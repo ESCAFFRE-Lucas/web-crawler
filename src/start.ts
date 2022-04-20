725aab298f3b229cd31225cf2ef6bfc6c23759a7
LESCAFFRE <lucas.escaffre@ynov.com>
2022-04-20 11:40:25 +0200

    try to get more information in hyperplanning and message for weekend

--- src/start.ts
+++ src/start.ts
@@ -12,21 +12,24 @@ import { bot } from './navy-bot';
     waitUntil: 'networkidle2',
   });
   await (await page.waitForSelector('input[id="username"]')).type('lucas.escaffre@ynov.com');
-  await (await page.waitForSelector('input[id="password"]')).type('Lucas20030309');
+  await (await page.waitForSelector('input[id="password"]')).type(process.env.PASSWORD);
   await (await page.waitForSelector('input[value="Connexion"]')).click();
-  await page.waitForSelector('div[class="EmploiDuTemps_Element AvecMain"]');
-  const trueSubject = await page.$eval('table[class="Cours  Texte10"]', async (ele: Element) => {
-    if (window.getComputedStyle(ele).left === '375px' && dayName === 'Vendredi') {
-      return await page.page.$$eval('#id_98_cont4 > div:not(:nth-child(3))', (ele:  Element[]) => {
-        return ele.map((item) => {
-          console.log(item.innerHTML);
-          return item.innerHTML;
-        });
-      });
-    }
-  });
-  console.log(trueSubject);
-  const subject = await page.$$eval('#id_98_cont0 > div:not(:nth-child(3))', (ele:  Element[]) => {
+  // const trueSubject = await page.$$eval('#GInterface\\.Instances\\[1\\]\\.Instances\\[9\\]_Grille_Elements', async (ele: Element[]) => {
+  //   console.log(...ele);
+  //   // ele.forEach((allitem) => {
+  //   //   if (window.getComputedStyle(allitem).left === '375px' && dayName === 'Dimanche') {
+  //   //     return page.$$eval('#id_98_cont4 > div:not(:nth-child(3))', (ele:  Element[]) => {
+  //   //       return ele.map((item) => {
+  //   //         console.log(item.innerHTML);
+  //   //         return item.innerHTML;
+  //   //       });
+  //   //     });
+  //   //   }
+  //   // })
+  // });
+  // console.log(trueSubject);
+  await page.waitForSelector('#id_98_cont2')
+  const subject = await page.$$eval('#id_98_cont2 > div:not(div:nth-child > label)', (ele:  Element[]) => {
     return ele.map((item) => {
       return item.innerHTML;
     });
