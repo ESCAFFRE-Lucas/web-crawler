71dd871887c25b4463447dd3a8c75be47ace0dd1
LESCAFFRE <lucas.escaffre@ynov.com>
2022-04-25 09:16:50 +0200

    not ping everyone for test, take the last element from the array (the class number)

--- src/start.ts
+++ src/start.ts
@@ -14,8 +14,21 @@ import { bot } from './navy-bot';
   await (await page.waitForSelector('input[id="username"]')).type('lucas.escaffre@ynov.com');
   await (await page.waitForSelector('input[id="password"]')).type(process.env.PASSWORD);
   await (await page.waitForSelector('input[value="Connexion"]')).click();
-  await page.waitForSelector('#id_98_cont2')
-  const subject = await page.$$eval('#id_98_cont2 > div:not(:nth-child(3))', (ele:  Element[]) => {
+  // await page.waitForSelector('div[id="GInterface.Instances[1].Instances[9]_Grille_Elements"]');
+  // const trueSubject = await page.$$eval('div[id="GInterface.Instances[1].Instances[9]_Grille_Elements"]', async (ele: Element[]) => {
+  //   ele.map(() => {
+  //       return page.$$eval('#id_98_cont4 > div:not(:nth-child(3))', (ele:  Element[]) => {
+  //         console.log(...ele);
+  //         return ele.map((item) => {
+  //           console.log(item.innerHTML);
+  //           return item.innerHTML;
+  //         });
+  //       });
+  //   })
+  // });
+  // console.log(trueSubject);
+  await page.waitForSelector('#id_143_cont3')
+  const subject = await page.$$eval('#id_143_cont3 > div:not(div:nth-child(5) > label)', (ele:  Element[]) => {
     return ele.map((item) => {
       return item.innerHTML;
     });
