722cf3e48ff33199689a228c3bb134d05829812a
LESCAFFRE <lucas.escaffre@ynov.com>
2022-04-29 11:24:47 +0200

    take the good data from the good day

--- src/start.ts
+++ src/start.ts
@@ -1,35 +1,67 @@
 const puppeteer = require('puppeteer');
-import { main } from './script';
+import { main, prisma } from './script';
 import { bot } from './navy-bot';
 
 (async () => {
   let days = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
   let d = new Date();
   let dayName = days[d.getDay()];
+  let getYdays = await prisma.agenda.findMany({
+    select: {matiere: true}
+  })
+  console.log(Object.values(getYdays[getYdays.length-1]));
+
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto('https://hp21.ynov.com/TOU', {
     waitUntil: 'networkidle2',
+    timeout: 0,
   });
   await (await page.waitForSelector('input[id="username"]')).type('lucas.escaffre@ynov.com');
   await (await page.waitForSelector('input[id="password"]')).type(process.env.PASSWORD);
   await (await page.waitForSelector('input[value="Connexion"]')).click();
-  const allDays = (await page.waitForXPath('/html/body/div[3]/div[2]/div[2]/div/table/tbody/tr[2]/td/div[1]/div[1]/div[2]/div[2]/div/div/div[1]/div[4]'))
-  const coursesDay = await allDays.$$eval('div', (ele:  Element[]) => {
-    return ele.map((item) => {
-      return item.innerHTML;
-    });
-  })
-  console.log(coursesDay);
-  const courses = (await page.waitForXPath('/html/body/div[3]/div[2]/div[2]/div/table/tbody/tr[2]/td/div[1]/div[1]/div[2]/div[2]/div/div/div[1]/div[4]/div[1]/div/table/tbody/tr/td'))
+  // const allDays = (await page.waitForXPath('/html/body/div[3]/div[2]/div[2]/div/table/tbody/tr[2]/td/div[1]/div[1]/div[2]/div[2]/div/div/div[1]/div[4]'))
+  // const coursesDay = await allDays.$$eval('div', (ele:  Element[]) => {
+  //   return ele.map((item) => {
+  //     return item.innerHTML;
+  //   });
+  // })
+  // console.log(coursesDay);
+  let i;
+  switch (dayName) {
+    case 'Lundi':
+      i = 1;
+      break;
+    case 'Mardi':
+      i = 2;
+      break;
+    case 'Mercredi':
+      i = 3;
+      break;
+    case 'Jeudi':
+      i = 4;
+      break;
+    case 'Vendredi':
+      i = 6;
+      break;
+  }
+
+  if (Object.values(getYdays[getYdays.length-1]) === ['Ydays']) {
+    switch (dayName) {
+      case 'Jeudi':
+        i = 5;
+        break;
+      case 'Vendredi':
+        i = 6;
+        break;
+    }
+  }
+  const courses = (await page.waitForXPath(`/html/body/div[3]/div[2]/div[2]/div/table/tbody/tr[2]/td/div[1]/div[1]/div[2]/div[2]/div/div/div[1]/div[4]/div[${i}]/div/table/tbody/tr/td`))
   const subject = await courses.$$eval('div', (ele:  Element[]) => {
     return ele.map((item) => {
       return item.innerHTML;
     });
   })
-  console.log(subject[0]);
-  console.log(subject[1]);
-  console.log(subject[2]);
   await main(subject);
   await bot(subject);
 })();
