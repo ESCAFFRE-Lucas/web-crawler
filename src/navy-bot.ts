ce09f10695a08e6313de23f843d34449769a4ace
LESCAFFRE <lucas.escaffre@ynov.com>
2022-04-29 11:31:33 +0200

    syntaxe problem

--- src/navy-bot.ts
+++ src/navy-bot.ts
@@ -21,19 +21,19 @@ export const bot = (arr: string[]) => {
         let dayName = days[d.getDay()];
         console.log(`Online as ${client.user.tag}`);
 
-        new schedule.scheduleJob('00 41 14 * * *', () => {
+        new schedule.scheduleJob('00 31 11 * * *', () => {
             if (dayName === 'Samedi' || dayName === 'Dimanche') {
                 client.channels.cache.get('964164221321490462').send('<@&966690322291769434>');
-                client.channels.cache.get('964164221321490462').send('Aujourdhui pas de cours !! Profitez bien de votre ' +
+                client.channels.cache.get('964164221321490462').send("Aujourd'hui pas de cours !! Profitez bien de votre " +
                     'week-end pour voir vos proches et travailler !! :) ');
             } else {
                 client.channels.cache.get('964164221321490462').send('<@&966690322291769434>');
                 client.channels.cache.get('964164221321490462').send('Attention à ne pas oublier le cours !!');
                 if (!isNaN(Number(arr[arr.length-1]))) {
-                    client.channels.cache.get('964164221321490462').send('Aujourdhui cest ' + dayName + ' vous avez '
+                    client.channels.cache.get('964164221321490462').send("Aujourd'hui c'est " + dayName + ' vous avez '
                         + arr[0] + ' avec ' + arr[1] + ' en salle ' + Number(arr[arr.length-1]));
                 } else {
-                    client.channels.cache.get('964164221321490462').send('Aujourdhui cest ' + dayName + ' vous avez '
+                    client.channels.cache.get('964164221321490462').send("Aujourd'hui c'est " + dayName + ' vous avez '
                         + arr[0] + ' avec ' + arr[1] + ' en salle : Inconnu ');
                 }
             }
